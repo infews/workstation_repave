cc5aedcb4fa07db54cbe89ac338d512c634a8c54
Davis W. Frank <dwfrank@pivotal.io>
2018-05-20 13:53:06 -0700

    WIP: Brewfile together, but Homebrew not happy with current dev deps

--- install/brew.sh
+++ install/brew.sh
@@ -3,8 +3,7 @@
 set -e
 source assets/bash_colors_ext.sh
 
-# Install brew
-function install_brew {
+function install_homebrew {
   if ! which brew > /dev/null ; then
     clr_green "Installing Homebrew"
     mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
@@ -13,4 +12,27 @@ function install_brew {
 	fi
 }
 
-install_brew
+function symlink_brewfile {
+  brewfile="${PWD}/assets/Brewfile"
+  brewfile_link="${HOME}/.Brewfile"
+
+  if [ ! -L "${brewfile_link}" ]; then
+    clr_green "Creating Brewfile symlink"
+    ln -s "${brewfile}" "${brewfile_link}"
+  else
+    clr_yellow "Brewfile symlink present, moving on"
+  fi
+
+}
+
+function update_and_install_all_from_brewfile {
+  clr_green "Updating Brew & installing everything from Brewfile"
+  brew update
+  brew tap Homebrew/bundle
+  brew bundle --global
+  brew cleanup
+}
+
+install_homebrew
+symlink_brewfile
+update_and_install_all_from_brewfile

53e2eb9d4c7be6b3b8eaaa771d60ffecac405298
Davis W. Frank <dwfrank@pivotal.io>
2018-05-24 16:13:06 -0700

    Change system name

---
+++ actors/system_name.rb
@@ -0,0 +1,30 @@
+require 'rainbow'
+using Rainbow
+
+class SystemName
+
+  @@name = 'redwood'
+
+  def SystemName.setup
+
+    Rainbow("Setting system name to #{@@name}").green
+
+    good_result = system "sudo scutil --set ComputerName #{@@name}"
+    return good_result unless good_result
+
+    good_result = system "sudo scutil --set HostName #{@@name}"
+    return good_result unless good_result
+
+    good_result = system "sudo scutil --set LocalHostName #{@@name}"
+    return good_result unless good_result
+
+    good_result = system "sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string #{@@name}"
+
+    return good_result
+  end
+end
+
+
+
+
+# Set computer name (as done via System Preferences → Sharing)

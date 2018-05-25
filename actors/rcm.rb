9633175d62540b47b7babbc09c80097b28a9fc3e
Davis W. Frank <dwfrank@gmail.com>
2018-05-24 20:01:51 -0700

    Fixing bad interpolation in RCM

--- actors/rcm.rb
+++ actors/rcm.rb
@@ -3,7 +3,7 @@ using Rainbow
 
 class Rcm
   def Rcm.setup
-    Rainbow('Setting up RCM to manage dotfiles in ').green + 'assets'
-    system "rcup -d #{File.join, Dir.home, 'balboa_workspace/assets'}"
+    puts Rainbow('Setting up RCM to manage dotfiles in ').green + 'assets'
+    system "rcup -d #{File.join(Dir.home, 'balboa_workspace/assets')}"
   end
 end

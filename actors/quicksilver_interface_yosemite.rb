07efe53b85ae2e1689cc0ccce16b500e2c51406c
Davis W. Frank <dwfrank@gmail.com>
2018-05-30 16:00:38 -0700

    Fix so we save the file we curl

--- actors/quicksilver_interface_yosemite.rb
+++ actors/quicksilver_interface_yosemite.rb
@@ -6,8 +6,8 @@ class QSInterfaceYosemite
   def QSInterfaceYosemite.do(path)
     Rainbow("Installing Quicksilver 'Yosemite' interface")
     Dir.chdir File.join(Dir.home, 'Downloads') do
-      system "curl -L https://github.com/mikker/YosemiteInterface-qsplugin/releases/download/v1.0.3/Yosemite-1.0.3.qsplugin.zip"
-      system "unzip Yosemite-1.0.3.qsplugin.zip"
+      system "curl -LO https://github.com/mikker/YosemiteInterface-qsplugin/releases/download/v1.0.3/Yosemite-1.0.3.qsplugin.zip"
+      system "unzip Yosemite-1.0.3.qsplugin"
       system "open Yosemite.qsplugin"
     end
   end

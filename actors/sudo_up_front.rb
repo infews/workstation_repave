02dd2cf7a8953bfd2016d4243346876e1a2299a8
Davis W. Frank <dwfrank@gmail.com>
2020-08-14 15:25:07 -0700

    Remove actors we've already replaced, or no longer need on >= Catalina

--- actors/sudo_up_front.rb
+++
@@ -1,9 +0,0 @@
-require 'rainbow'
-using Rainbow
-
-class SudoUpFront
-  def SudoUpFront.do(path)
-    puts Rainbow("Running ").mediumpurple + "sudo " + Rainbow("early so shell has permissions").mediumpurple
-    return system "sudo -v"
-  end
-end
